d09b15887bcb872a1f070e464efd22c6ee5646a1
Klaus <reverty@gmail.com>
2014-08-27 10:43:38 +0400

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,17 +11,12 @@
 $config = array();
 
 $cfgSetTagCallbackFull = Config::Get('jevix.default.cfgSetTagCallbackFull');
-$cfgSetTagCallbackFull[] = array(
-								'blockquote',
-								array('_this_','CallbackBlockquote'),
-							);
-Config::Set('jevix.default.cfgSetTagCallbackFull', $cfgSetTagCallbackFull);
-
 $cfgAllowTagParams = Config::Get('jevix.default.cfgAllowTagParams');
-$cfgAllowTagParams[] = array(
-							'blockquote',
-							array('comment'=>'#int')
-						);
+
+$cfgSetTagCallbackFull[] = array('blockquote', array('_this_','CallbackBlockquote'));
+$cfgAllowTagParams[] = array('blockquote', array('comment'=>'#int'));
+
+Config::Set('jevix.default.cfgSetTagCallbackFull', $cfgSetTagCallbackFull);
 Config::Set('jevix.default.cfgAllowTagParams', $cfgAllowTagParams);
 
 return $config;
